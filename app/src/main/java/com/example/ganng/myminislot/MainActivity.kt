2778b7213932f5be9056f68e698d27caaea1aa37
ganngiNote <ganngi77@gmail.com>
2018-11-08 10:19:59 +0900

    1st commit

---
+++ app/src/main/java/com/example/ganng/myminislot/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.ganng.myminislot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.preference.PreferenceManager
+
+
+class MainActivity : AppCompatActivity() {
+    val defultCoin = 1000;
+    val defultbet = 10;
+    var coin = 0;
+    var bet = 10;
+//    val prefer = PreferenceManager.getDefaultSharedPreferences(this);
+//    val editor = prefer.edit()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val prefer = PreferenceManager.getDefaultSharedPreferences(this);
+        val editor = prefer.edit()
+        setContentView(R.layout.activity_main)
+
+        coin = prefer.getInt("MY_COIN", 1000)
+        my_coin1.setText(coin.toString())
+        my_bet1.setText(bet.toString())
+
+
+        up_bet.setOnClickListener {bet_upTapped(it)}
+        down_bet.setOnClickListener {bet_downTapped(it)}
+        reset_btn.setOnClickListener { reset(it) }
+        start.setOnClickListener { intent(it) }
+
+
+    }
+
+    fun bet_upTapped(view: View?){
+        if(coin <= 0){
+
+        }else{
+            coin = coin - 10;
+            bet = bet + 10
+            my_coin1.setText((coin).toString())
+            my_bet1.setText((bet).toString())
+        }
+    }
+
+    fun bet_downTapped(view: View?){
+        if(bet == 10){
+
+        }else{
+            coin = coin + 10
+            bet = bet -10
+            my_coin1.setText((coin).toString())
+            my_bet1.setText((bet).toString())
+        }
+    }
+    fun reset(view: View?){
+        val prefer = PreferenceManager.getDefaultSharedPreferences(this);
+        val editor = prefer.edit()
+        coin = defultCoin
+        bet = defultbet
+        my_coin1.setText((coin).toString())
+        editor.putInt("MY_COIN",defultCoin)
+        editor.commit()
+    }
+    fun intent(view: View?){
+        val intent = Intent(this, SubActivity::class.java)
+        intent.putExtra("coin" , coin)
+        intent.putExtra("bet", bet)
+        startActivity(intent)
+    }
+
+}
---
+++ app/src/main/java/com/example/ganng/myminislot/SubActivity.kt
@@ -0,0 +1,82 @@
+package com.example.ganng.myminislot
+
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.sub_activity.*
+
+class SubActivity : AppCompatActivity(){
+    var coin = 0;
+    var bet = 0;
+    val array = arrayOf(R.drawable.banana,R.drawable.bar,R.drawable.bigwin,R.drawable.cherry
+        , R.drawable.grape, R.drawable.lemon, R.drawable.orange
+        , R.drawable.seven, R.drawable.waltermelon)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sub_activity)
+
+
+
+        coin = intent.getIntExtra("coin",0)
+        bet = intent.getIntExtra("bet", 0)
+
+        my_coin1.setText(coin.toString())
+        my_bet1.setText(bet.toString())
+        btn.setOnClickListener { stop(it) }
+        modoru.setOnClickListener { modoru(it) }
+    }
+
+    fun stop(view: View?){
+        val bt1 = (Math.random() * 9).toInt()
+        iv_1.setImageResource(array[bt1])
+        val bt2 = (Math.random() * 9).toInt()
+        iv_2.setImageResource(array[bt2])
+        val bt3 = (Math.random() * 9).toInt()
+        iv_3.setImageResource(array[bt3])
+
+        if(coin >= bet){
+            if (bt1 == bt2 && bt2 == bt3) {
+
+                if(bt1 == 7){
+                    coin = coin + (bet * 30)
+                }else if(bt1 == 3){
+                    coin = coin + (bet * 10)
+                }else if(bt1 == 2){
+                    coin = coin + (bet * 5)
+                }else{
+                    coin = coin + (bet * 3)
+                }
+
+            }else if(bt1 == bt2 || bt2 == bt3 || bt1 == bt3){
+                coin = coin + (bet * 1)
+
+            }else if(bt1 == 9 || bt2 == 9 || bt3 == 9){
+                coin = coin * (bet * 1)
+            }else if(bt1 == 7 && bt2 == 4 && bt3 == 6){
+                coin = coin * (bet * 30)
+            }else if(bt1 == 9 && bt2 == 1 && bt3 == 5){
+                coin = coin * (bet * 10)
+            }else{
+                coin = coin - bet
+            }
+
+            my_coin1.setText(coin.toString())
+        }
+
+    }
+
+    fun modoru(view: View?){
+        val prefer = PreferenceManager.getDefaultSharedPreferences(this);
+        val editor = prefer.edit()
+        coin = coin
+        editor.putInt("MY_COIN",coin)
+        editor.commit()
+        finish()
+    }
+
+
+
+
+}
